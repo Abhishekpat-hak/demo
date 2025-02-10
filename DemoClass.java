82fa0414f8aae754f5e2879fca0efe7787e425c3
Abhishek Pathak <66110825+Abhishekpat-hak@users.noreply.github.com>
2025-02-10 18:09:27 +0530

    Update DemoClass.java

--- DemoClass.java
+++ DemoClass.java
@@ -1,6 +1,20 @@
 public class DemoClass {
     public static void main(String[] args) {
 
-        System.out.println("THis is a demo github project!");
+        System.out.println("This is a demo github project!");
+        int number = 20;
+        DemoClass demo = new DemoClass();
+        String result =   demo.verifyUser(number);
+        System.out.println(result);
+
+
+    }
+
+    public String verifyUser(int number) {
+        if (number % 2 == 0) {
+            return "User is Correct";
+        } else {
+            return "User Incorrect";
+        }
     }
 }
